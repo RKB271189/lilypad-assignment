f5d138ef7b7f9d68edb314cd1737f5ed4ac3080f
Rahul Baraiya <rahul.baraiya05@gmail.com>
2025-04-08 11:15:51 -0600

    setting up bootstrap

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,17 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  
-
   return (
     <>
-      <div>
-        Hello World
+      <div class="container">
+        <h1 class="text-center mt-5">Welcome to Lily Pad Assignment</h1>
+        <button class="btn btn-primary">Click Me</button>
       </div>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
